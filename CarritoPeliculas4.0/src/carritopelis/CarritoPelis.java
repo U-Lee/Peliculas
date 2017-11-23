a893a8b1daa4276c4fe7dfc8a79bc9526ad06a31
U-Lee <32946965+U-Lee@users.noreply.github.com>
2017-11-23 13:20:50 -0600

    Add files via upload

---
+++ CarritoPeliculas4.0/src/carritopelis/CarritoPelis.java
@@ -0,0 +1,21 @@
+package carritopelis;
+
+/**
+ *
+ * @author Omar Ulises Hernández Cervantes, Oswaldo Josue Hernández Juárez
+ */
+public class CarritoPelis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPeliculas ventana = new VentanaPeliculas();
+        ventana.setTitle("Compra peliculas");
+        ventana.conexionBD();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+        ventana.setResizable(false);
+    }
+
+}
